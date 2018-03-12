03be0f4007136da3531bf6584e9dda39df959a62
mcqueenna <35352127+mcqueenna@users.noreply.github.com>
2018-03-12 17:53:59 +1100

    removed empty space

--- restart-vmware-service2.ps1
+++ restart-vmware-service2.ps1
@@ -1,13 +1,9 @@
-
 stop-service vimQueryService -force
 Stop-Service VMwareSTS -force
 Stop-Service VMwareIdentityMgmtService -force
-
 
 start-Service VMwareIdentityMgmtService
 start-Service VMwareSTS
 start-service vimQueryService
 
 start-service vpxd
-
-
